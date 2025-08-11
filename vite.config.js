6be85cacd64ea0d55f47889e4adfc7af8b189f28
Q-A-S-I-M <qasimali7618@gmail.com>
2025-08-11 22:45:19 +0500

    New Commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+base: '/Mimi-Notes/',
   plugins: [react()],
   server: {
     host: true, // enables LAN access
